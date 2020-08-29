d4cf03697bdee6cf52b3563b68a3a4317027a6f4
henry <henry.odiete@opin.ca>
2020-08-29 14:44:00 -0400

    Updates

---
+++ precache-manifest.2c7c6be6bb6e71da7ac1895d07986115.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d592e7c5c5741742eaacda211e5b02de",
+    "url": "/workorders/index.html"
+  },
+  {
+    "revision": "d2139589e73c4b40b447",
+    "url": "/workorders/static/css/main.7db7d1be.chunk.css"
+  },
+  {
+    "revision": "b11271575b7a6ae67570",
+    "url": "/workorders/static/js/2.7b3a9ace.chunk.js"
+  },
+  {
+    "revision": "d2139589e73c4b40b447",
+    "url": "/workorders/static/js/main.37d9a1f6.chunk.js"
+  },
+  {
+    "revision": "f08509830df74576f8ed",
+    "url": "/workorders/static/js/runtime-main.9e30823b.js"
+  }
+]);
